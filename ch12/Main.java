ff20adf29c4fba334d8de79647122ceafc1ec549
toshi-bp <yttoshi-bp@outlook.jp>
2021-12-18 17:45:50 +0900

    ch08 && ch12

--- ch12/Main.java
+++ ch12/Main.java
@@ -19,5 +19,36 @@ public class Main {
             '/'
         );
         b4.show();
+
+        // 問題12-1
+        b2 = new UpDownBorder(b1, '-');
+        b4 = new FullBorder(
+            new UpDownBorder(
+                new SideBorder(
+                    new UpDownBorder(
+                        new SideBorder(
+                            new StringDisplay("こんにちは。"),
+                            '*'
+                        ),
+                        '='
+                    ),
+                    '|'
+                ),
+                '/'
+            )
+        );
+        b4.show();
+
+        // 問題12-2
+        MultiStringDisplay md = new MultiStringDisplay();
+        md.add("おはようございます。");
+        md.add("こんにちは。");
+        md.add("おやすみなさい、また明日。");
+        md.show();
+
+        Display d1 = new SideBorder(md, '#');
+        d1.show();
+        Display d2 = new FullBorder(md);
+        d2.show();
     }
 }
---
+++ ch12/UpDownBorder.java
@@ -0,0 +1,29 @@
+package ch12;
+
+public class UpDownBorder extends Border{
+    private char borderChar;
+    public UpDownBorder(Display display, char ch) {
+        super(display);
+        this.borderChar = ch;
+    }
+    public int getColumns() {
+        return display.getColumns();
+    }
+    public int getRows() {
+        return 1 + display.getRows() + 1;
+    }
+    public String getRowText(int row) {
+        if (row == 0 || row == getRows() - 1) {
+            return makeLine(borderChar, getColumns());
+        } else {
+            return display.getRowText(row - 1);
+        }
+    }
+    private String makeLine(char ch, int count) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < count; i++) {
+            buf.append(ch);
+        }
+        return buf.toString();
+    }
+}
