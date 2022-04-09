462b2c7fcd5da6c670ac6a7ea3316aa8c9e161b6
toshi-bp <yttoshi-bp@outlook.jp>
2022-04-09 23:07:57 +0900

    add HtmlWriter class

--- ch15/pagemaker/HtmlWriter.java
+++ ch15/pagemaker/HtmlWriter.java
@@ -18,4 +18,22 @@ public class HtmlWriter {
         writer.write("<body>\n");
         writer.write("<h1>" + title + "</h1>\n");
     }
+    // 段落の出力
+    public void paragraph(String msg) throws IOException {
+        writer.write("<p>" + msg + "</p>\n");
+    }
+    // リンクの出力
+    public void link(String href, String caption) throws IOException {
+        paragraph("<a href=\"" + href + "\">" + caption + "</a>");
+    }
+    // メールアドレスの出力
+    public void mailto(String mailaddr, String username) throws IOException {
+        link("mailto:" + mailaddr, username);
+    }
+    // 閉じる
+    public void close() throws IOException {
+        writer.write("</body>");
+        writer.write("</html>\n");
+        writer.close();
+    }
 }
