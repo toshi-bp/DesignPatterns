2e5041b4751effff3daed7c15643137e96cdb840
toshi-bp <yttoshi-bp@outlook.jp>
2022-04-09 23:01:38 +0900

    add title method

--- ch15/pagemaker/HtmlWriter.java
+++ ch15/pagemaker/HtmlWriter.java
@@ -1,5 +1,21 @@
 package ch15.pagemaker;
 
+import java.io.Writer;
+import java.io.IOException;
+
 public class HtmlWriter {
-    
+    private Writer writer;
+    // ↓コンストラクタ
+    public HtmlWriter(Writer writer) {
+        this.writer = writer;
+    }
+    // タイトルの出力
+    public void title(String title) throws IOException {
+        writer.write("<html>");
+        writer.write("<head>");
+        writer.write("<title>" + title + "</title>");
+        writer.write("</head>");
+        writer.write("<body>\n");
+        writer.write("<h1>" + title + "</h1>\n");
+    }
 }
